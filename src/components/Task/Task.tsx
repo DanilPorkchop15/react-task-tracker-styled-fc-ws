fe2ec91a681a82dd3e1e0a21fbc23867c79e2137
Danil Kostin <danilkostin150705@gmail.com>
2024-09-23 14:26:00 +0300

    feat(fc migration): migrate task to fc

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,8 +1,7 @@
-import React, { Component } from "react";
+import React, { FC, useState, useEffect } from "react";
 import { ITask } from "../../types/Task.types";
 import "./Task.css";
 import { fetchUser } from "../../services/User.service";
-import { IUser } from "../../types/User.types";
 import { updateTask } from "../../services/Task.service";
 import TaskEdit from "../TaskEdit/TaskEdit";
 
@@ -11,120 +10,83 @@ interface ITaskProps extends ITask {
   onToggle: (id: number) => void;
 }
 
-type ITaskState = {
-  doEdit: boolean;
-  username: string | null;
-  title: string;
-};
+const Task: FC<ITaskProps> = ({
+  id,
+  title,
+  completed,
+  userId,
+  onDelete,
+  onToggle,
+}) => {
+  const [doEdit, setDoEdit] = useState(false);
+  const [username, setUsername] = useState<string | null>(null);
+  const [newTitle, setNewTitle] = useState(title);
 
-class Task extends Component<ITaskProps, ITaskState> {
-  constructor(props: ITaskProps) {
-    super(props);
-    this.state = {
-      doEdit: false,
-      username: null,
-      title: this.props.title,
-    };
-  }
-
-  private handleToggle: () => void = () => {
-    this.props.onToggle(this.props.id);
-  };
-  private handleDelete: () => void = () => {
-    this.props.onDelete(this.props.id);
-  };
-  private handleEdit: (title: string, username: number) => void = (
-    title,
-    userId
-  ) => {
+  useEffect(() => {
     fetchUser(userId)
-      .then(({ username }) => {
-        updateTask({
-          id: this.props.id,
-          title,
-          completed: this.props.completed,
-          userId,
-        });
-        this.setState({
-          ...this.state,
-          doEdit: !this.state.doEdit,
-          title,
-          username,
-        });
+      .then(({ username }) => setUsername(username))
+      .catch((e: Error) => console.log("User fetch error " + e));
+  }, [userId]);
+
+  const handleToggle = () => onToggle(id);
+  const handleDelete = () => onDelete(id);
+  const handleEdit = (title: string, userId: number) => {
+    fetchUser(userId)
+      .then(({ username }: { username: string }) => {
+        updateTask({ id, title, completed, userId });
+        setDoEdit(false);
+        setNewTitle(title);
+        setUsername(username);
       })
-      .catch((e: Error) => {
-        console.log("Task edit error " + e);
-      });
+      .catch((e: Error) => console.log("Task edit error " + e));
   };
 
-  private toggleEdit = () => {
-    this.setState({ doEdit: !this.state.doEdit });
-  };
-  componentDidMount(): void {
-    fetchUser(this.props.userId)
-      .then((user: IUser) => {
-        this.setState({ username: user.username });
-      })
-      .catch((e: Error) => {
-        console.log("User fetch error " + e);
-      });
-  }
+  const toggleEdit = () => setDoEdit(!doEdit);
 
-  render(): React.ReactNode {
-    return (
-      <div className="task fl-col j-between">
-        <section className="task-section fl a-baseline j-between">
+  return (
+    <div className="task fl-col j-between">
+      <section className="task-section fl a-baseline j-between">
+        <div className="task-block fl a-baseline">
+          <span>{id}</span>
+          <p>{newTitle}</p>
+        </div>
+
+        <div className="task-block fl a-baseline">
           <div className="task-block fl a-baseline">
-            <span>{this.props.id}</span>
-            <p>{this.state.title}</p>
+            <span>{username ? username : "loading..."}</span>
           </div>
 
           <div className="task-block fl a-baseline">
-            <div className="task-block fl a-baseline">
-              <span>
-                {this.state.username ? this.state.username : "loading..."}
-              </span>
-            </div>
-
-            <div className="task-block fl a-baseline">
-              <input
-                type="checkbox"
-                name="completed"
-                id="completed"
-                onChange={this.handleToggle}
-                checked={this.props.completed}
-              />
-              <div className="task-actions fl">
-                <button
-                  className="task-edit button"
-                  onClick={this.toggleEdit}
-                  disabled={this.state.username === null}
-                >
-                  {this.state.doEdit ? "Cancel" : "Edit"}
-                </button>
-                <button
-                  className="task-delete button"
-                  onClick={this.handleDelete}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section className="task-section fl a-baseline j-between">
-          {this.state.doEdit && (
-            <TaskEdit
-              onEdit={this.handleEdit}
-              title={this.props.title}
-              username={this.state.username}
+            <input
+              type="checkbox"
+              name="completed"
+              id="completed"
+              onChange={handleToggle}
+              checked={completed}
             />
-          )}
-        </section>
-      </div>
-    );
-  }
-}
+            <div className="task-actions fl">
+              <button
+                className="task-edit button"
+                onClick={toggleEdit}
+                disabled={username === null}
+              >
+                {doEdit ? "Cancel" : "Edit"}
+              </button>
+              <button className="task-delete button" onClick={handleDelete}>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="task-section fl a-baseline j-between">
+        {doEdit && (
+          <TaskEdit onEdit={handleEdit} title={title} username={username} />
+        )}
+      </section>
+    </div>
+  );
+};
 
 export default Task;
